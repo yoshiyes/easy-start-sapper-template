9770f2a856af605d161bfe39bc011a5e64e1a30b
Karim <20050570+yoshiyes@users.noreply.github.com>
2020-09-02 09:41:39 +0200

    Add base project

---
+++ src/routes/index.svelte
@@ -0,0 +1,59 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<script lang="typescript">
+	import { Col, Container, Row } from 'sveltestrap';
+
+    export let title: string = "Hello TypeScript";
+</script>
+
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<Container>
+  <Row>
+	<Col>
+		<p class="text-center">HELLO</p>
+	</Col>
+  </Row>
+</Container>
+
+<figure>
+	<img alt='Success Kid' src='successkid.jpg'>
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
